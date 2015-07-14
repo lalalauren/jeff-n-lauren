aec803de50d6854abd09c0d31858b0c2baefe63e
lalalauren <lauren@jeff-n-lauren.com>
2015-07-14 12:24:39 -0400

    Adding the RSVP javascript

---
+++ js/rsvp_autocomplete.js
@@ -0,0 +1,46 @@
+$(function() {
+  /**
+   * Autocomplete functionality for dropdown.
+   */
+  $("#name").autocomplete({
+    minLength: 3,
+    select: function(event, ui) {
+      if (ui.item) {
+        // Set the input.
+        $('#name').val(ui.item.label);
+        
+        // Create an overlay.
+        $('body').append('<div id="overlay"></div>');
+        
+        // Define the dialog box.
+        var box = window.getBox(ui.item.hidden, ui.item.label);
+        $('body').append(box);
+
+        // Define a closing div and set it up to work.
+        $('#overlay').append(window.overlayClose);
+      }
+    },
+    source: function(request, response) {
+      $.ajax({
+        type: 'POST',
+        dataType: 'json',
+        url: 'rsvp_autocomplete.php?name=' + request.term,
+        success: function(data) {
+          response(
+                  $.map(data, function(item) {
+                    return {
+                      label: item.title + " " + item.firstname + " " + item.lastname + (item.suffix === undefined ? "" : " " + item.suffix),
+                      hidden: item.gid
+                    };
+                  })
+                  );
+        },
+        error: function(request, error) {
+          console.log(response);
+          console.log(request);
+          console.log("ERROR WITH AUTOCOMPLETE: " + error);
+        }
+      });
+    }
+  });
+});
---
+++ js/rsvp_post.js
@@ -0,0 +1,111 @@
+$(function() {
+  /**
+   * Hide RSVP timestamps unless we're hovering.
+   */
+  $('a.registered-guest').hover(
+          function() {
+            var time = $(this).children('span.rsvp-timestamp');
+            if (time.css('display') === 'none') {
+              time.slideDown().delay(1000).slideUp();
+            }
+          }
+  );
+
+  /**
+   * When clicking on a guest not yet registered, launch a dialog box for them.
+   */
+  $('.unregistered-guest').click(function() {
+    // Create an overlay.
+    $('body').append('<div id="overlay"></div>');
+
+    // Create the dialog box.
+    var box = window.getBox($(this).attr('name'), $(this).html());
+    $('body').append(box);
+
+    // Define a closing div and set it up to work.
+    $('#overlay').append(window.overlayClose);
+  });
+
+  /**
+   * Get the dialog box for registering.
+   * 
+   * @param int gid
+   *   Guest identifier.
+   * @param string name
+   *   Formatted guest name.
+   *   
+   * @returns {$}
+   *   Jquery HTML for the dialog box.
+   */
+  function getBox(gid, name) {
+    // Define the Yes button.
+    var yes = $('<a id="rsvp-yes" class="rsvp-button" src="#">Yes</a>');
+    yes.hover(function() {
+      $(this).css('border-color', '#66FF66');
+    }, function() {
+      $(this).css('border-color', '#fff');
+    });
+    // Determine the function for the Yes button.
+    yes.click(function() {
+      // Submit the form.
+      $.ajax({
+        type: "POST",
+        cache: false,
+        url: 'rsvp_post.php',
+        data: {'gid': gid},
+        success: function(data) {
+          // Remove the input field.
+          $('input#name').remove();
+          if (data) {
+            // Put new text in.
+            $('#rsvp-form').replaceWith(data);
+          } else {
+            // Put new text in.
+            $('#rsvp-form').replaceWith('There was an error registering '
+                    + name + '. Contact Jeff or Lauren directly.');
+          }
+        }
+      });
+      // Remove the content box.
+      $('.overlay-content').remove();
+      // Remove the overlay background.
+      $("#overlay").remove();
+    });
+
+    // Define the No button.
+    var no = $('<a id="rsvp-no" class="rsvp-button" src="#">No</a>');
+    no.hover(function() {
+      $(this).css('border-color', '#FF0000');
+      $(this).css('cursor', 'pointer');
+    }, function() {
+      $(this).css('border-color', '#fff');
+      $(this).css('cursor', 'default');
+    });
+    // Determine the function for the no button.
+    no.click(function() {
+      // Remove the content box.
+      $('.overlay-content').remove();
+      // Remove the overlay background.
+      $("#overlay").remove();
+    });
+
+    // Define the box.
+    var box = $('<div class="overlay-content" id="overlay-content-rsvp">'
+            + 'Are you sure you want to RSVP '
+            + '<br/>'
+            + '"' + name + '"?'
+            + '<br/>'
+            + '</div>');
+
+    // Append the options to the box.
+    box.append(yes);
+    box.append(no);
+
+    // Return the HTML for the box.
+    return box;
+  }
+
+  // Make the getBox function available globally outside of this file so that
+  // the autocomplete can use it too.
+  window.getBox = getBox;
+});
