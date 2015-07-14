02c02074e5a7adfe6f22281741599bea57bc29ff
lalalauren <lauren@jeff-n-lauren.com>
2015-07-14 12:30:17 -0400

    Adding header/footer

---
+++ footer.php
@@ -0,0 +1,3 @@
+<div id="footer">
+  Please check back frequently for updates. Last Updated: May 15, 2015.
+</div>
---
+++ header.php
@@ -0,0 +1,19 @@
+<div id="header">
+  <div id="strings"></div>
+  <h1 id="pagetitle" class="title">Jeff & Lauren</h1>
+  <div id="subtitle" class="title">September 12, 2015</div>
+  <div id="menu">
+    <div id="little-menu">
+      <span class="title" style="float: left;">
+        Jeff & Lauren
+      </span>
+      <span id="open-menu" style="float: right;"></span>
+      <div class="clearfix"></div>
+    </div>
+    <div id="navigation">
+      <a href="#about-the-wedding">Our Ceremony & Reception</a>
+      <a href="#about-the-venue">About the Venue</a>
+      <a href="#directions">Directions</a>
+    </div>
+  </div>
+</div>
