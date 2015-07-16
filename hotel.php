203e25de65d20818a784d76353444153be5432f0
lalalauren <lauren@jeff-n-lauren.com>
2015-07-16 11:42:11 -0400

    Updating the hotel page and its css

--- hotel.php
+++ hotel.php
@@ -1,5 +1,68 @@
-We have reserved a block of hotel rooms at the <b>Hampton Inn & Conference Center</b>.
-We will be providing
-<a class="nav-link" name="shuttle">shuttle service</a>
-from there to the venue, so we would love
-it if you booked there.
+<p>
+  We will be providing
+  <br/>
+  <a class="nav-link" name="shuttle">shuttle service</a>
+  from the
+  <br/>
+  <b>Hampton Inn & Conference Center</b>.
+</p>
+<br/>
+
+<div id="hotel-block">
+  <div id="hotel-block-info">
+    <div class="title">Hampton Inn & Conference Center</div>
+    <div id="hotel-address">1204 Berryville Ave., Winchester, VA 22601</div>
+  </div>
+
+  <div id="hotel-image">
+    <img src="images/hotel.jpg"/>
+  </div>
+
+  <div class="clearfix"></div>
+</div>
+
+<div id="must-book">
+  <div id="days-left">
+    <div id="days-left-number"><?php echo floor((strtotime('September 1, 2015') - time()) / 86400); ?></div>
+    <div id="days-left-text">days left</div>
+  </div>
+  <div>Must book rooms by 09/01/2015!</div>
+</div>
+
+<div id="hotel-block2">
+  <div>
+    Stay Date is <br/>
+    <span class="date">Saturday, September 12, 2015</span>
+    through 
+    <span class="date">Sunday, September 13, 2015</span>.
+  </div>
+
+  <br/><br/>
+  To confirm your rooms, you must call Reservations at <span class="date">1-540-678-4000</span> 
+  and say you want to confirm with the <span class="date">Corron Wedding</span> group. 
+  <br/><br/><br/>
+
+  We have the following type of rooms blocked:<br/>
+
+  <table>
+    <thead>
+      <tr>
+        <th>Price</th>
+        <th>Room Type</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>$91.00</td>
+        <td>1 King or Queen Bed</td>
+      </tr>
+      <tr>
+        <td>$91.00</td>
+        <td>2 Double Beds</td>
+      </tr>
+    </tbody>
+  </table>
+
+  <br/>For more information, check out 
+  <a href="https://corronwedding.hotelplanner.com/" target="_blank">Expedia's page</a>.
+</div>
