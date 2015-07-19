99eaeadfaf02dca996c7b7ca3aeac6c903e8bd54
lalalauren <lauren@jeff-n-lauren.com>
2015-07-19 10:25:14 -0400

    Changing register text to RSVP; adding the libraries and the access lock

--- rsvp_post.php
+++ rsvp_post.php
@@ -1,7 +1,14 @@
 <?php
 
+// Allow access.
+$access = 'access_lock';
+
 // Require the config file.
 require_once 'config.php';
+// Include the libraries.
+foreach (glob('utils/*.php') as $util) {
+  include_once $util;
+}
 
 // Do not continue processing if we are not connected to the database.
 if (!$connect) {
@@ -10,7 +17,8 @@ if (!$connect) {
 }
 
 // RSVP someone.
-$gid = $_REQUEST['gid'];
+$gid = check_plain($_REQUEST['gid']);
+$attending = check_plain($_REQUEST['attending']);
 
 // If this is not a valid gid, do not return a succecss.
 if (!get_guest($gid)) {
@@ -20,27 +28,27 @@ if (!get_guest($gid)) {
 // Create the SQL.
 $ip = getIP();
 $time = time();
-$sql = "INSERT INTO rsvped (gid, ip, timestamp)
-VALUES ('" . $gid . "', '" . $ip . "', '" . $time . "')";
+$sql = "INSERT INTO rsvped (gid, attending, ip, timestamp)
+VALUES ('" . $gid . "', '" . $attending . "', '"  . $ip . "', '" . $time . "')";
 
 // Insert the RSVP.
 if (mysql_query($sql)) {
   $guest = get_guest($gid);
-  echo "Registered " 
+  echo "RSVPed " 
     . to_guest_name($guest)
     . ' ' . format_rsvp_time($time) . '.';
   if (($other_guest = get_other_guest($gid))) {
     if ( !is_rsvped($other_guest['gid'])) {
-    echo ' Did you want to register '
-      . '<a class="unregistered-guest" name="' . $other_guest['gid'] . '">'
+    echo ' Did you want to RSVP '
+      . '<a class="not-rsvped-guest" name="' . $other_guest['gid'] . '">'
     . to_guest_name($other_guest) 
             . '</a>'
             . ' too?';
     }
     else {
-      echo ' You have already registered ' 
+      echo ' You have already RSVPed ' 
       . to_guest_name($other_guest) . ' previously.'
-              . ' Please contact Lauren or Jeff if you need to register another guest.';
+              . ' Please contact Lauren or Jeff if you need to R another guest.';
     }
   }
 } else {
