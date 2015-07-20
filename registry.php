0ae3c75f0a1052ca9238fdb74af58a001059cecb
lalalauren <lauren@jeff-n-lauren.com>
2015-07-20 08:10:32 -0400

    Jeff's changes to the registry

--- registry.php
+++ registry.php
@@ -1,14 +1,13 @@
 <p>
-  We are very fortunate to already have many items for our home. As you may know, 
-  we appreciate the small daily things and enjoy practical gifts. We would love 
+  We are fortunate to already have many of the typical household items. As you may know, 
+  we appreciate the small daily things and would enjoy practical gifts. We would love 
   and welcome gift cards from the places we shop or eat at routinely: <br/>
   Amazon, Giant Food, Home Depot, Target, Wylie Wagg, etc
 </p>
 <p>
   We have put together a traditional registry on 
   <a target="_blank" href="http://www.bedbathandbeyond.com/store/giftregistry/view_registry_guest.jsp?pwsToken=&eventType=Wedding&registryId=542374453">Bed, Bath, & Beyond</a>
-  for some
-  practical items we do not yet own and some wish list items that would we love.
+  for some practical items and a few on the wish list that we would love.
   Tater has also helped us put together a list on 
   <a target="_blank" href="http://www.amazon.com/registry/wedding/G2KQQSLYJMTE">Amazon</a>.
 </p>
